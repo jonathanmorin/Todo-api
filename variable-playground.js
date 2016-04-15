2781a477e34169203878cf4f5f6801e8369a5b12
John Morin <inscription@cochez.ca>
2016-04-15 11:37:39 -0400

    add new file

---
+++ variable-playground.js
@@ -0,0 +1,34 @@
+// var person = {
+// 	name: 'John',
+// 	age: 21
+// };
+
+
+
+// function updatePerson(obj) {
+// 	// obj = {
+// 	// 	name: 'John',
+// 	// 	age: 33
+// 	// };
+
+// 	obj.age = 33
+
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+
+// Array Example
+
+var grades = [15,88
+];
+
+function addGrades (Array) {
+	 Array.push(55);
+	// Array = [12,33,99];
+	debugger;
+}
+
+addGrades(grades);
+console.log(grades);
